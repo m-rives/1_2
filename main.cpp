8655f6aabcdef301e7e9ccaa30e5a571b724486b
Marc Rives <marc.rives@edu.umit.at>
2020-10-24 10:41:48 +0200

    getline + delete dashes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
+//HUE 1.2
+
+//ISBN: Berechnung der Pruefziffer
+
+//Mattia Huber Oberfrank    01417215
+//Marc Rives                XXXXXXXX
+
 
 #include <string>
 #include <vector>
 #include <iostream>
 #include <sstream>
 #include <ostream>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
 
 int main()
 {
+    string isbn;        //declare variables
+    string isbn_raw;
+
+    cout << "" << endl;     //obvious cout is obvious
+    cout << "This algorithm provides the calculation of the checkdigit for incomplete ISBN-13 codes." << endl;
+    cout << "" << endl;
+    cout << "The code must contain exactly 12 digits." << endl;
+    cout << "" << endl;
+    cout << "Please enter the ISBN code without any spaces:" << endl;
+
+    getline(cin,isbn);       //read ISBN from console
+
+    isbn_raw = isbn;                //save original isbn for final output
+
+    cout << "isbn_raw:" << isbn_raw << endl;
+    boost::erase_all(isbn, "-");
+    cout << "isbn:" << isbn << endl;
+
 
 }
