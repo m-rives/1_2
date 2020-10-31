13532606bd97506ee54de00c487266f916d3102a
S1ck <mattia.huber-oberfrank@edu.umit.at>
2020-10-31 10:04:35 +0100

    it works but I probably didnt understand the exercise.. fuck it

--- main.cpp
+++ main.cpp
@@ -19,27 +19,40 @@
 
 
 using namespace std;
-
-int main()
-{
-    int K, n, m = 365, Z = 0;
-
-    cout << "" << endl;
-    cout << "Please enter the amount of people you would like to simulate: " << endl;
-    cin >> n;
-    //cout << "" << endl;
-    //cout << "Please enter the amount of days in a year you would like to simulate (Earth:265; Mars:670; Saturn:24475): " << endl;
-    //cin >> m;
-    cout << "" << endl;
-    cout << "Please enter the amount of groups you would like to simulate: " << endl;
-    cin >> K;
+/*
+vector <int> calc2 (){
 
 
 
+    for (int j = 0; j < KK; j++) {
+
+        for (int i = 0; i < nn; i++) {
+            randomNumber2[i] = (rand() % mm) + 1;
+
+        }
+        sort(randomNumber2.begin(), randomNumber2.end());
+        for (int c = 0; c < randomNumber2.size(); c++) {
+
+            while (randomNumber2[c] == randomNumber2[c + 1]) {
+                ZZ++;
+                c++;
+
+            }
+
+        }
+        //cout << ZZ << endl;
+        //cout << "For group " << j + 1 << " there are " << ZZ << " matching birthdays." << endl;
+        amount2.push_back(ZZ);
+        ZZ = 0;
+
+
+    }
+}*/
+
+vector <int> calc (int K, int n, int m){
+    int Z = 0;
     vector <int> amount(K,0);
-
     vector <int> randomNumber(n,0);
-
     for(int j =0; j < K; j++) {
 
         for (int i = 0; i < n; i++) {
@@ -52,6 +65,7 @@ int main()
             while (randomNumber[c] == randomNumber[c+1]){
                 Z++;
                 c++;
+
             }
 
         }
@@ -61,8 +75,62 @@ int main()
         Z = 0;
 
     }
+    for(int h = 0; h < amount.size(); h++ ){
+
+        if (amount[h] != 0){
+            amount[h] = 1;
+        }
+    }
+    return amount;
+}
+
+int main()
+{
+    int K, n;
+    int m = 365;
+
+            cout << "" << endl;
+    cout << "Please enter the amount of people you would like to simulate: " << endl;
+    cin >> n;
+    cout << "" << endl;
+    cout << "Please enter the amount of groups you would like to simulate: " << endl;
+    cin >> K;
+    vector <int> amount = calc(K,n,m);
+
     double mean = ((std::accumulate(amount.begin(), amount.end(), 0.0))/(K*n))*100;
     cout << "On average " << mean << "% of "<< n << " people in the given " << K << " Groups have their Birthdays on the same day." <<endl;
     int sum = (std::accumulate(amount.begin(), amount.end(), 0.0));
     cout << "There were " << sum << " matching cases." <<endl;
+    cout << "" << endl;
+
+    int KK;
+    int mm;
+    cout << "Please enter the amount of groups you would like to simulate: " << endl;
+    cin >> KK;
+    char choice;
+    cout << "Enter M for Mars; S for Saturn" << endl;
+    cin >> choice;
+    if (choice = 'M'){
+        mm = 670;
+    }
+    if (choice = 'S'){
+        mm = 22475;
+    }
+    double ZM = (KK/2);
+    double sum2 = 0.0;
+    int nn = 5;
+    vector <int> randomNumber2(nn,0);
+    vector <int> amount2(KK,0);
+
+    while (sum2 < (ZM +1)) {
+        nn++;
+
+        //cout << sum2 << endl;
+        //cout << ZM << endl;
+        //cout << nn << endl;
+        vector <int> amount2 = calc (KK,nn,mm);
+        sum2 = ((std::accumulate(amount2.begin(), amount2.end(), 0.0)));
+        cout << "" << endl;
+        cout << nn << " people are neeede to fulfill the 50% requirement." << endl;
+    }
 }
