08efd224d405abfb463cb6cdc207c0d7bcb68a20
S1ck <mattia.huber-oberfrank@edu.umit.at>
2020-11-01 10:06:13 +0100

    final

--- main.cpp
+++ main.cpp
@@ -19,20 +19,20 @@
 
 using namespace std;
 
-vector <int> calc (int K, int n, int m){
+vector <int> calc (int K, int n, int m){        //function to get amount of collisions in birthdays
     int Z = 0;
     vector <int> amount(K,0);
     vector <int> randomNumber(n,0);
-    for(int j =0; j < K; j++) {
+    for(int j =0; j < K; j++) {                 //for loop through all groups
 
-        for (int i = 0; i < n; i++) {
-            randomNumber[i] = (rand() % m) + 1;
+        for (int i = 0; i < n; i++) {           //for loop through all people
+            randomNumber[i] = (rand() % m) + 1;     //random number generator from 1 to m
 
         }
-        sort(randomNumber.begin() , randomNumber.end());
+        sort(randomNumber.begin() , randomNumber.end());    //sort generated numbers
         for (int c = 0; c < randomNumber.size(); c++){
 
-            while (randomNumber[c] == randomNumber[c+1]){
+            while (randomNumber[c] == randomNumber[c+1]){       //get counter Z
                 Z++;
                 c++;
 
@@ -45,7 +45,7 @@ vector <int> calc (int K, int n, int m){
         Z = 0;
 
     }
-    for(int h = 0; h < amount.size(); h++ ){
+    for(int h = 0; h < amount.size(); h++ ){        //delete double entries
 
         if (amount[h] != 0){
             amount[h] = 1;
@@ -59,7 +59,7 @@ int main()
     int K, n;
     int m = 365;
 
-            cout << "" << endl;
+    cout << "" << endl;
     cout << "Please enter the amount of people you would like to simulate: " << endl;
     cin >> n;
     cout << "" << endl;
@@ -67,9 +67,9 @@ int main()
     cin >> K;
     vector <int> amount = calc(K,n,m);
 
-    double mean = ((std::accumulate(amount.begin(), amount.end(), 0.0))/(K*n))*100;
+    double mean = ((std::accumulate(amount.begin(), amount.end(), 0.0))/(K*n))*100;     //calculate average/mean
     cout << "On average " << mean << "% of "<< n << " people in the given " << K << " Groups have their Birthdays on the same day." <<endl;
-    int sum = (std::accumulate(amount.begin(), amount.end(), 0.0));
+    int sum = (std::accumulate(amount.begin(), amount.end(), 0.0));                     //sum off all cases
     cout << "There were " << sum << " matching cases." <<endl;
     cout << "" << endl;
 
@@ -77,13 +77,13 @@ int main()
     int mm;
     cout << "Please enter the amount of groups you would like to simulate: " << endl;
     cin >> KK;
-    char choice;
+    char choice;            //choice between mars and saturn
     cout << "Enter M for Mars; S for Saturn" << endl;
     cin >> choice;
-    if (choice = 'M'){
+    if (choice = 'M'){      //choice for mars
         mm = 670;
     }
-    if (choice = 'S'){
+    if (choice = 'S'){      //choice for saturn
         mm = 22475;
     }
     double ZM = (KK/2);
@@ -92,12 +92,9 @@ int main()
     vector <int> randomNumber2(nn,0);
     vector <int> amount2(KK,0);
 
-    while (sum2 < (ZM +1)) {
+    while (sum2 < (ZM +1)) {        //increase number of people until criteria is met
         nn++;
 
-        //cout << sum2 << endl;
-        //cout << ZM << endl;
-        //cout << nn << endl;
         vector <int> amount2 = calc (KK,nn,mm);
         sum2 = ((std::accumulate(amount2.begin(), amount2.end(), 0.0)));
         cout << "" << endl;
