ff85fe089c2e623bb902622d602b00211b1ba5c9
S1ck <mattia.huber-oberfrank@edu.umit.at>
2020-11-30 15:06:55 +0100

    submission

--- graphsearch.cpp
+++ graphsearch.cpp
@@ -5,48 +5,6 @@
 
 using namespace std;
 
-/*
-void breadthfirstsearch (Graph g, int s, vector <int> &p, vector <int> &d)
-{
-  cout << "Steps of depth-first search" << endl;
-  int n = g.get_nodecount ();
-  // Initialise arrays p and d, and FIFO queue for nodes to be processed
-  p.clear (); p.insert (p.begin(), n, -1);
-  d.clear (); d.insert (d.begin(), n, -1);
-  queue <int> q;
-  p[s] = s;
-  d[s] = 0;
-  q.push (s);
-  cout << "Initialising: d[" << s << "]=0, p[" << s << "]=" << s << endl;
-  // Main loop
-  int i = q.front();
-  while (!q.empty()) {
-
-    cout << "Processing node " << i << " from queue" << endl;
-    // Loop over outgoing edges of current node
-    for (int j : g.get_edges (i)) {
-      cout << "Following edge (" << i << ", " << j << "): ";
-      if (p[j] == -1) {
-        cout << "Node " << j << " not visited before; ";
-        d[j] = d[i] + 1;
-        p[j] = i;
-        q.push (j);
-        cout << "d[" << j << "]=" << d[j] << ", "
-             << "p[" << j << "]=" << p[j] << ", "
-             << "appending node " << j << " to queue" << endl;
-            i++;
-      }
-      else {
-        cout << "Node " << j << " visited before, no change" << endl;
-      }
-    }
-    q.pop ();
-  }
-  cout << endl;
-  return;
-}
-*/
-
 void depthfirstsearch(Graph g, int s, vector <int> &p)
 {
     cout << "Steps of depth-first search" << endl;
@@ -91,3 +49,45 @@ void subtreesearch(Graph g, int i, vector <int> &p)
 
     return;
 }
+
+/*
+void breadthfirstsearch (Graph g, int s, vector <int> &p, vector <int> &d)
+{
+  cout << "Steps of depth-first search" << endl;
+  int n = g.get_nodecount ();
+  // Initialise arrays p and d, and FIFO queue for nodes to be processed
+  p.clear (); p.insert (p.begin(), n, -1);
+  d.clear (); d.insert (d.begin(), n, -1);
+  queue <int> q;
+  p[s] = s;
+  d[s] = 0;
+  q.push (s);
+  cout << "Initialising: d[" << s << "]=0, p[" << s << "]=" << s << endl;
+  // Main loop
+  int i = q.front();
+  while (!q.empty()) {
+
+    cout << "Processing node " << i << " from queue" << endl;
+    // Loop over outgoing edges of current node
+    for (int j : g.get_edges (i)) {
+      cout << "Following edge (" << i << ", " << j << "): ";
+      if (p[j] == -1) {
+        cout << "Node " << j << " not visited before; ";
+        d[j] = d[i] + 1;
+        p[j] = i;
+        q.push (j);
+        cout << "d[" << j << "]=" << d[j] << ", "
+             << "p[" << j << "]=" << p[j] << ", "
+             << "appending node " << j << " to queue" << endl;
+            i++;
+      }
+      else {
+        cout << "Node " << j << " visited before, no change" << endl;
+      }
+    }
+    q.pop ();
+  }
+  cout << endl;
+  return;
+}
+*/
