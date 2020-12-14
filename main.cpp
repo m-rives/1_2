bf00da2fec76ba4b7d403f9f6512d2638b855dc6
Marc Rives <marc.rives@edu.umit.at>
2020-12-14 11:53:05 +0100

    didnt do shit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include "user_io.h"
 #include "imagec.h"
+#include "vector"
 
 using namespace std;
 
@@ -25,6 +26,8 @@ double mandelbrot_value (double cr, double ci, int kmax)
   while (zr * zr + zi * zi <= 4.0 && k < kmax) {
     ++k;
     // Compute z*z+c in (zrnew, zinew) and copy back
+    //double zrnew = zr * zr - zi * zi + cr;
+    //double zinew = 2.0 * zr * zi + ci;
     double zrnew = zr * zr - zi * zi + cr;
     double zinew = 2.0 * zr * zi + ci;
     zr = zrnew;
@@ -33,6 +36,36 @@ double mandelbrot_value (double cr, double ci, int kmax)
   // Scale final number of iterations by kmax; 1.0* avoids integer division!
   return 1.0 * k / kmax;
 }
+
+
+double iterFuncMap (double cr, double ci, int kmax) {
+
+    double xk_new = 0;
+    double xk = 0;
+    int k = 0;
+
+    while (xk <= 2 || k <= kmax) {
+
+        xk_new = cr * xk * (1 - xk);
+        k++;
+    }
+
+    return 1.0 * k / kmax;
+}
+
+
+vector< vector< int >> calcIter(double r_min, double r_max, double i_min, double i_max) {
+
+    //vector< vector< int >> myVec()
+    for (int i = i_min; i < i_max; ++i){
+        for (int r = r_min; r < r_max; ++r) {
+            xk = myVec[i-1][r];
+            xk_new = r*xk(1-xk);
+            myVec[i][r] = xk_new;
+        }
+    }
+}
+
 
 // Function to visualise the Mandelbrot function within a rectangular region;
 // generates a (grey-value) Image object with the function values 
@@ -55,7 +88,7 @@ Image mandelbrot_image (int nx,
       // Output mval as grey-value only if iteration not completed;
       // otherwise leave pixel black (inside Mandelbrot set)
       if (mval < 1.0)
-        output (i, j) = 255.0 * mandelbrot_value (x, y, k);
+        output (i, j) = 255.0 * iterFuncMap (x, y, k);
     }
   }
   return output;
@@ -64,18 +97,21 @@ Image mandelbrot_image (int nx,
 int main (int argc, char *argv[])
 {
   // Greeting
-  cout << "Computation of Mandelbrot fractals" << endl;
+  cout << "Computation of iterated function maps" << endl;
 
   // Obtain user input for fractal computation
-  int k = userinput_int ("Number of iterations:");
+  int k = userinput_int ("Number of iterations before display:");
+
+  int d = userinput_int ("Number of iterations to display:");
 
   // Obtain user inputs for plot range and output file name
-  double cr_min = userinput_double ("Drawing range start (real part):     ");
-  double cr_max = userinput_double ("Drawing range end   (real part):     ");
-  double ci_min = userinput_double ("Drawing range start (imaginary part):");
-  double ci_max = userinput_double ("Drawing range end   (imaginary part):");
+  double cr_min = userinput_double ("Drawing range start (parameter):     ");
+  double cr_max = userinput_double ("Drawing range end   (parameter):     ");
+  double ci_min = userinput_double ("Drawing range start (iterated values):");
+  double ci_max = userinput_double ("Drawing range end   (iterated values):");
   string outfilename = userinput ("Name of output image:");
   int nx = userinput_int ("Width of output image:");
+  int ny = userinput_int ("Height of output image:");
 
   // Perform computation
   Image fractalimage = 
